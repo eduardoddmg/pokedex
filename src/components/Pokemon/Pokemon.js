ecce0b7836c82eb9b5914e30bfedd5078ccff85c
eduardoddmg <eduardoddmg@gmail.com>
2022-04-08 08:42:43 -0300

    first commit

---
+++ src/components/Pokemon/Pokemon.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import "./pokemon.css";
+import { Link } from "react-router-dom";
+import { FaSyncAlt } from "react-icons/fa";
+
+
+const Pokemon = () => {
+  const [info, setInfo] = useState(null);
+
+  const fetching = () => {
+    fetch("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=2000")
+      .then((response) => response.json())
+      .then((data) => setInfo(() => data.results))
+      .catch((e) => console.log(e));
+  };
+
+  useEffect(() => {
+    fetching();
+    console.log(info);
+  }, [info]);
+
+
+  return (
+    <>
+      {!info && (
+        <section className="espera">
+          <FaSyncAlt className="loading" />
+          <p>Aguarde enquanto os dados carregam</p>
+        </section>
+      )}
+      {info && (
+        <h1 className="titulo">
+          {" "}
+          <img
+            src="https://www.freeiconspng.com/uploads/file-pokeball-png-0.png"
+            alt="logo"
+          />{" "}
+          Pokedex
+        </h1>
+      )}
+      <div className="container_principal">
+        {info &&
+          info.map((value, index) => {
+            return (
+              <Link
+                key={index}
+                className="container_pokemon"
+                to={`principal/${index + 1}`}
+              >
+                <h1>{value.name}</h1>
+                <img
+                  src={
+                    "https://unpkg.com/pokeapi-sprites@2.0.4/sprites/pokemon/other/dream-world/" +
+                    (index + 1) +
+                    ".svg"
+                  }
+                  alt="img"
+                />{" "}
+              </Link>
+            );
+          })}
+      </div>
+    </>
+  );
+};
+
+export default Pokemon;
---
+++ src/components/Principal/Principal.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import "./principal.css";
+import { FaSyncAlt } from "react-icons/fa";
+
+const Principal = () => {
+  const [dataText, setDataText] = useState(null);
+  const [dataHability, setDataHability] = useState(null);
+  let params = useParams();
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/ability/${params.id}`)
+      .then((response) => response.json())
+      .then((data) => setDataText(Object.values(data.effect_entries[1].effect)))
+      .catch((e) => console.log(e));
+  }, [dataText]);
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/characteristic/${params.id}`)
+      .then((response) => response.json())
+      .then((data) =>
+        setDataHability(Object.values(data.descriptions[7].description))
+      )
+      .catch((e) => console.log(e));
+  }, [dataHability]);
+
+  console.log(dataText);
+
+  return (
+    <>
+      {(!dataHability || !dataHability) && (
+        <section className="espera">
+          <FaSyncAlt className="loading" />
+          <p>Aguarde enquanto os dados carregam</p>
+        </section>
+      )}
+      {(dataHability && dataText) && (
+        <div className="principal">
+          <img
+            src={`https://unpkg.com/pokeapi-sprites@2.0.4/sprites/pokemon/other/dream-world/${params.id}.svg`}
+            alt="pokemon img"
+          />
+          <p>{dataText}</p>
+          <p>{dataHability}</p>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Principal;
